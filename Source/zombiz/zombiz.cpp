a2a82015bef9240ec39f2e2ce18ea041771619fe
MrSebi06 <mihneasebastian06@gmail.com>
2025-01-15 21:46:21 +0100

    Add everything

---
+++ Source/zombiz/zombiz.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "zombiz.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, zombiz, "zombiz" );
